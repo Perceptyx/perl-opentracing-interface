415c348e56ecca01bc91a3ac2cd1a58af498d834
Theo van Hoesel <Th.J.v.Hoesel+CPAN@gmail.com>
2019-12-19 10:28:14 +0100

    change `use` statments, reflecting renaming

--- lib/OpenTracing/Interface/SpanContext.pm
+++ lib/OpenTracing/Interface/SpanContext.pm
@@ -4,7 +4,7 @@ package OpenTracing::Interface::SpanContext;
 use strict;
 use warnings;
 
-use OpenTracing::ReadableInterface;
+use OpenTracing::Interface::Utils::RoleAround;
 
 use Types::Standard qw/Str Value/;
 
